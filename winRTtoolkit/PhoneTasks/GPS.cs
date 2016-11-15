684ed74a69554c097f49f3572b5be1adb82d79d3
Filipe Rodrigues <frodrigues@thing-pink.pt>
2016-11-15 21:05:07 +0000

    + PhoneTasks

---
+++ winRTtoolkit/PhoneTasks/GPS.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+
+namespace winRTtoolkit.PhoneTasks
+{
+    public class GPS
+    {
+        public async static Task<Geoposition> GetPositionAsync(int accuracy)
+        {
+            try
+            {
+                var geolocator = new Geolocator();
+                if (geolocator.LocationStatus == PositionStatus.Disabled)
+                    return null;
+
+                geolocator.DesiredAccuracyInMeters = Convert.ToUInt32(accuracy);
+                Geoposition position = await geolocator.GetGeopositionAsync();
+
+                return position;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                return null;
+            }
+        }
+    }
+}
---
+++ winRTtoolkit/PhoneTasks/Launcher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace winRTtoolkit.PhoneTasks
+{
+    public class Launcher
+    {
+        /// <summary>
+        /// Phone Task Launcher opening urls or specific services
+        /// </summary>
+        /// <param name="site/service url"></param>
+        public async static Task LaunchUriAsync(string uri)
+        {
+            try
+            {
+                await Windows.System.Launcher.LaunchUriAsync(new Uri(uri, UriKind.Absolute));
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+        }
+    }
+}
